e75f44d4adeb7fe7dcfafc80929df32c6c66cb6e
TariroMpofu <130043688+TariroMpofu@users.noreply.github.com>
2025-08-09 21:02:16 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -307,15 +307,18 @@ export default function Home() {
     <div
       className={`${spaceGrotesk.className} bg-black text-[#fefeff] flex flex-col min-h-screen`}
     >
-      <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-CZNM8V2BK5"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-CZNM8V2BK5');
-</script>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-CZNM8V2BK5"
+      />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-CZNM8V2BK5');
+        `}
+      </Script>
 
       <AnimatePresence mode="wait">
         {loading ? (
@@ -902,4 +905,5 @@ export default function Home() {
     </div>
   );
 }
+
 
